1c24970fa4c9f62868204c96065d0ee4de0b8419
dansoevans <70035391+dansoevans@users.noreply.github.com>
2022-12-05 08:49:01 +0000

    init

---
+++ main.js
@@ -0,0 +1,16 @@
+function goBack(){
+    window.history.back();
+
+}
+
+function promptHandler(){
+    var prompter = confirm( "      " + " DELETE ITEM \n  Do you want to delete item ? ");
+if(prompter == false){
+  event.preventDefault();
+}
+  return prompter;
+}
+var hide = docum
+function goHide(){
+
+}
